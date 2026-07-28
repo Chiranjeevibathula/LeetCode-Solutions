5c69e134fc3d6e910a0c01c06c785f4b4c1ad4ff
BATHULA CHIRANJEEVI <147606811+Chiranjeevibathula@users.noreply.github.com>
2026-07-28 15:59:51 +0000

    Sync LeetCode submission Runtime - 6 ms (56.65%), Memory - 26.4 MB (70.48%)

--- 0189-rotate-array/solution.py
+++ 0189-rotate-array/solution.py
@@ -1,7 +1,13 @@
 class Solution:
     def rotate(self, nums: List[int], k: int) -> None:
-        for _ in range(k):
-             a=nums.pop()
-             nums.insert(0,a)
-
+        def reverse(start:int,end:int)->None:
+            while start<end:
+                nums[start],nums[end]=nums[end],nums[start]
+                start+=1
+                end-=1        
+        n=len(nums)
+        k = k % n
+        reverse(0,n-1)
+        reverse(0,k-1)
+        reverse(k,n-1)        
         
